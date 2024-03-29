94db71754054ba2018644c076844c5788b29e561
asfandyar-malik <asfandyarashraf@hotmail.com>
2024-03-29 14:26:49 +0100

    Change to bright blue color

--- src/components/common/WidgetWrapper.tsx
+++ src/components/common/WidgetWrapper.tsx
@@ -2,7 +2,7 @@ import { twMerge } from 'tailwind-merge';
 import { WrapperTagProps } from '~/shared/types';
 import Background from './Background';
 
-const WidgetWrapper = ({ children, id, hasBackground, containerClass }: WrapperTagProps) => (
+const WidgetWrapper = ({ children, id, hasBackground=false, containerClass }: WrapperTagProps) => (
   <section className="relative not-prose scroll-mt-[72px]" id={id}>
     <Background hasBackground={hasBackground} />
     <div
