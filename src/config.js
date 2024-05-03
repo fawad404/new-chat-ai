37e8774057039447c86a8f12408e61f1ec17e2d6
asfandyar-malik <asfandyarashraf@hotmail.com>
2024-05-03 15:27:00 +0200

    asdf

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 module.exports.SITE = {
-  name: 'ChatSite Enterprise',
+  name: 'ChatSite',
 
   origin: 'https://tailnext.vercel.app',
   basePathname: '/',
   trailingSlash: false,
 
-  title: 'ChatSite Enterprise',
+  title: 'ChatSite',
   description: 'Custom ChatGPT on your private data, in a secure way and can be hosted on your server, without leaving your walls',
 };
