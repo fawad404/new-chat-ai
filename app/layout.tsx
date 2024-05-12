02156e0fcfe21effc9439c1eaac59c92685447c5
asfandyar-malik <asfandyarashraf@hotmail.com>
2024-05-12 11:20:20 +0200

    sdf

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,14 +31,18 @@ export default function RootLayout({ children }: LayoutProps) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="google-site-verification" content="9R74iVP1lIOhW2T4PMG38s7oLM7trFsesu-Ic4XZpgI" />
-
+ 
       </head>
       <body className="tracking-tight antialiased text-gray-900 dark:text-slate-300">
+        
         <Providers>
           <Header />
           <main>{children}</main>
           <Footer2 />
         </Providers>
+
+        <script id="__webwhizSdk__" src="https://widget.webwhiz.ai/webwhiz-sdk.js" data-chatbot-id="661e48c07abb6768aec10b14"></script>
+        
       </body>
     </html>
   );
